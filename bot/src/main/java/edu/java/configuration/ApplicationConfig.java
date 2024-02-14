7ed9b512b6f15e6f6ae440c47726142b7b5e01da
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-14 13:36:14 +0300

    Remove TelegramBot-name property

--- bot/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -8,8 +8,6 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotEmpty
-    String telegramToken,
-    @NotEmpty
-    String telegramName
+    String telegramToken
 ) {
 }
