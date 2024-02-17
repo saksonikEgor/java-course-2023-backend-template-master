b0037b837fa3e5a56af90e68dabe44f715ff870e
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-17 16:15:14 +0300

    Remove Nullable annotation

--- bot/src/main/java/edu/java/util/URIUtils.java
+++ bot/src/main/java/edu/java/util/URIUtils.java
@@ -4,13 +4,14 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class URIUtils {
     private URIUtils() {
     }
 
-    public static @Nullable URI castStringToURI(String uri) throws URISyntaxException, MalformedURLException {
+    public static URI castStringToURI(String uri) throws URISyntaxException, MalformedURLException {
         return new URL(uri).toURI();
     }
 }
