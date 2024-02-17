ddc799cd5e626ece46572780096722c4ac14bf0a
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-17 16:24:03 +0300

    Correct style

--- bot/src/main/java/edu/java/util/URIUtils.java
+++ bot/src/main/java/edu/java/util/URIUtils.java
@@ -4,8 +4,6 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class URIUtils {
     private URIUtils() {
