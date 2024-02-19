1df3db2ce4d6ab1f639365fe8459e78d17522123
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-19 12:01:07 +0300

    Fix getting base-url from config

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -2,23 +2,26 @@ package edu.java.configuration;
 
 import edu.java.client.GitHubClient;
 import edu.java.client.StackOverflowClient;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
-@ConfigurationProperties(prefix = "client")
+@Setter
 public class ClientConfiguration {
     @Value("${github.base-url:https://api.github.com}")
-    private String githubBaseURL;
+    private String gitHubBaseURL;
     @Value("${stackoverflow.base-url:https://api.stackexchange.com}")
     private String stackOverflowBaseURL;
 
     @Bean
     public GitHubClient gitHubClient() {
-        return new GitHubClient(buildClientByBaseURL(githubBaseURL));
+        return new GitHubClient(buildClientByBaseURL(gitHubBaseURL));
     }
 
     @Bean
@@ -26,7 +29,7 @@ public class ClientConfiguration {
         return new StackOverflowClient(buildClientByBaseURL(stackOverflowBaseURL));
     }
 
-    private WebClient buildClientByBaseURL(String baseURL) {
+    private @NotNull WebClient buildClientByBaseURL(String baseURL) {
         return WebClient.builder()
             .baseUrl(baseURL)
             .build();
