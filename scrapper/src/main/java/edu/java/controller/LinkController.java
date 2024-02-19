fb1a87c8af753e3086c363de71c0bd294e3f7b18
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-19 16:22:07 +0300

    Add scrapper API controllers

---
+++ scrapper/src/main/java/edu/java/controller/LinkController.java
@@ -0,0 +1,99 @@
+package edu.java.controller;
+
+import edu.java.dto.request.AddLinkRequest;
+import edu.java.dto.request.RemoveLinkRequest;
+import edu.java.dto.response.APIErrorResponse;
+import edu.java.dto.response.LinkResponse;
+import edu.java.dto.response.ListLinksResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/links")
+public class LinkController {
+    @GetMapping
+    @Operation(summary = "Получить все отслеживаемые ссылки")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ссылки успешно получены",
+                     content = @Content(schema = @Schema(implementation = ListLinksResponse.class),
+                                        mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json"))
+    })
+    public ResponseEntity<ListLinksResponse> getLinks(@RequestHeader("Tg-Chat-Id") long chatId) {
+        log.info("Getting links for chat: " + chatId);
+
+        ListLinksResponse response = new ListLinksResponse(
+            List.of(new LinkResponse(1, "https://gist.github.com/sanyarnd/52a78a01fa9ec234c8ad50fbc5ecc9e4")),
+            1
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    @Operation(summary = "Добавить отслеживание ссылки")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ссылка успешно добавлена",
+                     content = @Content(schema = @Schema(implementation = LinkResponse.class),
+                                        mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json"))
+    })
+    public ResponseEntity<LinkResponse> addLink(
+        @RequestHeader("Tg-Chat-Id") long chatId,
+        @RequestBody AddLinkRequest request
+    ) {
+        log.info("Adding link for chat: " + chatId + ", and request: " + request);
+
+        LinkResponse linkResponse = new LinkResponse(
+            1,
+            "https://gist.github.com/sanyarnd/52a78a01fa9ec234c8ad50fbc5ecc9e4"
+        );
+
+        return ResponseEntity.ok(linkResponse);
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Убрать отслеживание ссылки")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ссылка успешно убрана",
+                     content = @Content(schema = @Schema(implementation = LinkResponse.class),
+                                        mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "Ссылка не найдена",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json"))
+    })
+    public ResponseEntity<LinkResponse> deleteLink(
+        @RequestHeader("Tg-Chat-Id") long chatId,
+        @RequestBody RemoveLinkRequest request
+    ) {
+        log.info("Deleting link for chat: " + chatId + ", and request: " + request);
+
+        LinkResponse linkResponse = new LinkResponse(
+            1,
+            "https://gist.github.com/sanyarnd/52a78a01fa9ec234c8ad50fbc5ecc9e4"
+        );
+
+        return ResponseEntity.ok(linkResponse);
+    }
+}
---
+++ scrapper/src/main/java/edu/java/controller/TelegramChatController.java
@@ -0,0 +1,51 @@
+package edu.java.controller;
+
+import edu.java.dto.response.APIErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/tg-chat")
+public class TelegramChatController {
+    @PostMapping("/{id}")
+    @Operation(summary = "Зарегестрировать чат")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Чат зарегистрирован",
+                     content = @Content),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json"))
+    })
+    public ResponseEntity<?> registerChat(@PathVariable("id") long chatId) {
+        log.info("Registering for charId: " + chatId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Удалить чат")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Чат успешно удалён",
+                     content = @Content),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "Чат не существует",
+                     content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                                        mediaType = "application/json"))
+    })
+    public ResponseEntity<?> deleteChat(@PathVariable("id") long chatId) {
+        log.info("Deleting chat by id: " + chatId);
+        return ResponseEntity.ok().build();
+    }
+}
