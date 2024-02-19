a1b15393b79eb8680c67c17ef78c80c9c3c2b887
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-19 14:26:12 +0300

    Add ConditionalOnProperty annotation

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -1,6 +1,7 @@
 package edu.java.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 @EnableScheduling
 @Slf4j
 @Component
+@ConditionalOnProperty(value = "app.scheduler.enable", havingValue = "true", matchIfMissing = true)
 public class LinkUpdaterScheduler {
     @Scheduled(fixedDelayString = "#{@schedulerInterval.toMillis()}")
     public void update() {
