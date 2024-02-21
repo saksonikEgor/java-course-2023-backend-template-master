ea53ca3a3fe34030433f4ced9a7071755f46127e
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-21 16:02:00 +0300

    Rename scrapper to integration

--- scrapper/src/test/java/edu/java/integration/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/integration/IntegrationTest.java
@@ -1,4 +1,4 @@
-package edu.java.scrapper;
+package edu.java.integration;
 
 import java.nio.file.Path;
 import java.sql.DriverManager;
--- scrapper/src/test/java/edu/java/integration/LiquibaseMigrationTest.java
+++ scrapper/src/test/java/edu/java/integration/LiquibaseMigrationTest.java
@@ -1,4 +1,4 @@
-package edu.java.scrapper;
+package edu.java.integration;
 
 import java.sql.ResultSet;
 import java.util.HashSet;
