fe0a5bb77ab8bb20ad1bbd226aff7db21f4da182
Saksonik Egor <saksonik.egor@mail.ru>
2024-02-29 15:45:00 +0300

    Correct deleted configuration from EnableConfigurationProperties

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,13 +1,12 @@
 package edu.java;
 
 import edu.java.configuration.ApplicationConfig;
-import edu.java.configuration.DatabaseConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ApplicationConfig.class, DatabaseConfiguration.class})
+@EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
