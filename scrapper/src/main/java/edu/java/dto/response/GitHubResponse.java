216e912e0f873c1c355fc4ccff7d4301841735bb
Saksonik Egor <saksonik.egor@mail.ru>
2024-03-05 10:33:05 +0300

    Correct deserializing github response

--- scrapper/src/main/java/edu/java/dto/response/GitHubResponse.java
+++ scrapper/src/main/java/edu/java/dto/response/GitHubResponse.java
@@ -8,7 +8,7 @@ import java.time.OffsetDateTime;
 public record GitHubResponse(
     @JsonProperty("full_name") String fullName,
     @JsonProperty("created_at") OffsetDateTime createdAt,
-    @JsonProperty("updated_at") OffsetDateTime updatedAt
+    @JsonProperty("pushed_at") OffsetDateTime updatedAt
 ) implements SiteAPIResponse {
     @Override
     public OffsetDateTime getLastUpdate() {
