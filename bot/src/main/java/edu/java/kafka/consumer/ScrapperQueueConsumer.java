280daf7ca6e795a63e1b6a9da987944f815b08c7
saksonikEgor <saksonik.egor@mail.ru>
2024-04-07 14:51:29 +0300

    Add consuming kafka messages

---
+++ bot/src/main/java/edu/java/kafka/consumer/ScrapperQueueConsumer.java
@@ -0,0 +1,23 @@
+package edu.java.kafka.consumer;
+
+import edu.java.bot.TelegramBotWrapper;
+import edu.java.dto.request.LinkUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ScrapperQueueConsumer {
+    private final TelegramBotWrapper telegramBotWrapper;
+
+    @KafkaListener(topics = "${app.kafka.topic.name}",
+                   groupId = "${spring.kafka.consumer.group-id}",
+                   concurrency = "${spring.kafka.listener.concurrency:2}")
+    public void listen(LinkUpdateRequest request) {
+        log.info("Received link update: {}", request);
+        telegramBotWrapper.sendMessages(request);
+    }
+}
